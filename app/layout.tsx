1caa49f1a5d5d6e66a170515b7ac64e702d37ed9
Peter Liu <peter@example.com>
2025-12-31 15:11:16 +0800

    Add Tailwind CSS setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import Link from "next/link";
 import {homePath, ticketsPath} from "./paths";
 import {Inter} from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({subsets: ["latin"]});
 
