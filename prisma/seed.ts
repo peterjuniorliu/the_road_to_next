355698939e23285fd058e66e38507cfc7b362b68
Peter Liu <peter@example.com>
2026-01-13 11:54:59 +0800

    solving database problems

---
+++ prisma/seed.ts
@@ -0,0 +1,59 @@
+import "dotenv/config";
+import { PrismaClient } from "../src/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const connectionString = process.env["DATABASE_URL"];
+if (!connectionString) {
+    throw new Error("DATABASE_URL is not set");
+}
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+const tickets = [
+    {
+        // id: "1",
+        title: "Ticket 1",
+        content: "First ticket from DB.",
+        status: "DONE" as const
+    },
+    {
+        // id: "2",
+        title: "Ticket 2",
+        content: "Second ticket from DB.",
+        status: "OPEN" as const
+    },
+    {
+        // id: "3",
+        title: "Ticket 3",
+        content: "Third ticket from DB.",
+        status: "IN_PROGRESS" as const
+    }
+];
+
+const seed = async () => {
+    const t0 = performance.now();
+
+    console.log("DB Seed: Started ...");
+
+    await prisma.ticket.deleteMany();
+    await prisma.ticket.createMany({
+        data: tickets,
+    });
+
+    const t1 = performance.now();
+
+    console.log(`DB Seed: Finished (${t1 - t0}ms)`);
+};
+
+seed()
+    .catch((error) => {
+        console.error(error);
+        process.exitCode = 1;
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+        await pool.end();
+    });
