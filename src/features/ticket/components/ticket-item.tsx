7700253d5bf2832471657d6e44bd3bfc4771bead
Peter Liu <peter@example.com>
2026-01-14 18:08:24 +0800

    adding route variable and normalize tickets page

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -15,7 +15,7 @@ const TicketItem = ({ticket, isDetail}: TicketItemProps) =>
 {
     const detailButton = (
         <Button variant="outline" size="icon">
-            <Link href={ticketPath(ticket.id)}>
+            <Link prefetch href={ticketPath(ticket.id)}>
                 <LucideArrowUpRightFromSquare className="w-4 h-4" />
             </Link>
         </Button>
